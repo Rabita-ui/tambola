a7b2388ca355d4ded26596a2d75b94f8eb119e4c
Rabita-ui <thongamrabitad@gmail.com>
2026-08-15 15:02:17 +0530

    Replace Firestore rules with Firebase configuration
    Removed Firestore security rules and added Firebase configuration.

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTGFdmmimUbpm1zrTqaSQBO0kr2LcWs8M",
+  authDomain: "mbolahouse.firebaseapp.com",
+  projectId: "mbolahouse",
+  storageBucket: "mbolahouse.firebasestorage.app",
+  messagingSenderId: "842446053948",
+  appId: "1:842446053948:web:b977bef8d84e85375f9939",
+  measurementId: "G-DP949M05J1"
+};
